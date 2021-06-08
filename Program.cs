63c46cdc6b7c0c6f4034cb7336eb028911ac05b9
ranabhinder1321 <ranabhinder1970@gmail.com>
2021-06-08 19:52:56 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Find_Greater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_1, number_2;
+
+
+
+            // Asks the user to enter a number
+            Console.WriteLine("Please enter first number");
+            //Below line converts the input from a string to an integer
+            number_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number");
+            number_2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (number_1 > number_2)
+            {
+                Console.WriteLine(number_1.ToString() + " is greater than " + number_2.ToString());
+            }
+
+
+
+            if (number_1 == number_2)
+            {
+                Console.WriteLine(number_1.ToString() + " is equal to " + number_2.ToString());
+            }
+
+
+
+            if (number_1 < number_2)
+            {
+                Console.WriteLine("Number 2 is greater than number 1");
+            }
+        }
+    }
+}
